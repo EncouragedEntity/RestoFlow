9d00bdb9b343e84f7ba1891c3d5823f402c5d6ea
EncouragedEntity <arturnitsevich938@gmail.com>
2023-09-10 17:36:08 +0300

    Initial commit

---
+++ lib/config/ngrok.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
+
+Future<Map<String, dynamic>> getTunnels() async {
+  const apiKey = '2VD136jTabE8TvIVfxSuTIwrcdJ_2n7JxpAi3b1s3SKT6Dfti';
+  final url = Uri.parse('https://api.ngrok.com/tunnels');
+  final response = await http.get(
+    url,
+    headers: {
+      'Authorization': 'Bearer $apiKey',
+      'Ngrok-Version': '2',
+    },
+  );
+  if (response.statusCode == 200) {
+    final jsonData = jsonDecode(response.body);
+    return jsonData;
+  } else {
+    Logger().log(Level.error, 'Error');
+    return {};
+  }
+}
+
+Future<String> getServerLink() async {
+  final tunnels = await getTunnels();
+  return tunnels['tunnels'][0]['public_url'];
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'config/ngrok.dart';
+
+void main() async {
+  final link = await getServerLink();
+
+  try {
+    Logger().log(
+      Level.info,
+      'app starts here',
+    );
+  } catch (e) {
+    Logger().log(
+      Level.error,
+      e.toString(),
+    );
+  }
+
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Text(
+          link,
+          style: const TextStyle(color: Colors.black),
+        ),
+      ),
+    ),
+  );
+}
