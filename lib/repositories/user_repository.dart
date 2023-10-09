593bbd89012c2f5cf94bace8321aec04923044d1
EncouragedEntity <arturnitsevich938@gmail.com>
2023-10-09 10:06:38 +0300

    User repository singleton conversion

--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -1,38 +1,59 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
-import '../models/user.dart';
+import 'package:resto_flow/data/constants.dart';
+import 'package:resto_flow/models/auth/user.dart';
 
 class UserRepository {
-  final String? apiUrl;
+  final String? hostname;
+  static UserRepository? instance;
+  static User? currentUser;
 
-  UserRepository({required this.apiUrl});
+  factory UserRepository({required String apiUrl}) {
+    instance ??= UserRepository._internal(hostname: apiUrl);
+    return instance!;
+  }
+
+  UserRepository._internal({required this.hostname});
 
   Future<User> logIn(String email, String password) async {
     final response = await http.post(
-      Uri.parse('$apiUrl/signin'),
+      Uri.parse(hostname! + authLoginUrl),
       body: jsonEncode({'email': email, 'password': password}),
-      headers: {'Content-Type': 'application/json'},
     );
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = jsonDecode(response.body);
-      return User.fromJson(data);
+      //TODO save user token
+      return currentUser!;
     } else {
       throw Exception('Failed to sign in: ${response.reasonPhrase}');
     }
   }
 
   Future<User> register(String email, String password) async {
+    final user = User(
+      username: email,
+      email: email,
+      firstName: "",
+      lastName: "",
+      authorities: ["USER"],
+      deleted: false,
+      password: password,
+      restaurantAddress: "",
+    );
+
+    final userJson = user.toJson();
+
     final response = await http.post(
-      Uri.parse('$apiUrl/signup'),
-      body: jsonEncode({'email': email, 'password': password}),
+      Uri.parse(hostname! + authRegisterUrl),
+      body: userJson,
       headers: {'Content-Type': 'application/json'},
     );
 
     if (response.statusCode == 201) {
       final Map<String, dynamic> data = jsonDecode(response.body);
-      return User.fromJson(data);
+      currentUser = User.fromJson(data);
+      return currentUser!;
     } else {
       throw Exception('Failed to sign up: ${response.reasonPhrase}');
     }
