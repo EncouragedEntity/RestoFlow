093fd40859ee0c40e4bd9902d37b2619ab4a6bd0
EncouragedEntity <arturnitsevich938@gmail.com>
2023-10-09 10:03:30 +0300

    navbar bloc

--- lib/blocs/nav_bloc.dart
+++ lib/blocs/nav_bloc.dart
@@ -3,21 +3,24 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../events/nav_event.dart';
 
 class NavBloc extends Bloc<NavEvent, int> {
-  NavBloc() : super(0) {
+  NavBloc() : super(2) {
     on<NavEvent>(
       (event, emit) {
         switch (event) {
-          case NavEvent.scanner:
+          case NavEvent.menu:
             emit(0);
             return;
-          case NavEvent.menu:
+          case NavEvent.order:
             emit(1);
             return;
-          case NavEvent.order:
+          case NavEvent.scanner:
             emit(2);
             return;
-          case NavEvent.profile:
+          case NavEvent.somePage: //TODO change
             emit(3);
+            return;
+          case NavEvent.profile:
+            emit(4);
             return;
         }
       },
--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -1,3 +1,4 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -7,41 +8,40 @@ import '../events/nav_event.dart';
 class NavBar extends StatelessWidget {
   const NavBar({
     super.key,
-    required this.selectedIndex,
+    this.initialIndex,
   });
 
-  final int selectedIndex;
-
+  final int? initialIndex;
   @override
   Widget build(BuildContext context) {
-    return Theme(
-      data: ThemeData(
-        canvasColor: Theme.of(context).backgroundColor,
-      ),
-      child: BottomNavigationBar(
-        currentIndex: selectedIndex,
-        onTap: (index) {
-          context.read<NavBloc>().add(NavEvent.values[index]);
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.qr_code),
-            label: 'Scan QR',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.restaurant_menu),
-            label: 'Menu',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.menu),
-            label: 'Order',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-      ),
+    return ConvexAppBar(
+      style: TabStyle.fixed,
+      initialActiveIndex: initialIndex,
+      onTap: (index) {
+        context.read<NavBloc>().add(NavEvent.values[index]);
+      },
+      items: const [
+        TabItem(
+          icon: Icon(Icons.restaurant_menu),
+          title: 'Menu',
+        ),
+        TabItem(
+          icon: Icon(Icons.menu),
+          title: 'Order',
+        ),
+        TabItem(
+          icon: Icon(Icons.qr_code_2),
+          title: 'Scan QR',
+        ),
+        TabItem(
+          icon: Icon(Icons.abc),
+          title: 'Some page',
+        ),
+        TabItem(
+          icon: Icon(Icons.person),
+          title: 'Profile',
+        ),
+      ],
     );
   }
 }
