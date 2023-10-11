56348297ddf3a48e11ef7bb9032c430793989e66
EncouragedEntity <arturnitsevich938@gmail.com>
2023-10-11 16:23:12 +0300

    Orientation lock

--- lib/main.dart
+++ lib/main.dart
@@ -5,51 +5,56 @@ import 'package:resto_flow/pages/home_page.dart';
 import 'package:resto_flow/repositories/user_repository.dart';
 import 'blocs/nav_bloc.dart';
 import 'config/ngrok.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
-  runApp(
-    MaterialApp(
-      theme: ThemeData(
-        backgroundColor: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: FutureBuilder(
-        future: getServerLink(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Scaffold(
-              body: Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  child: Text(
-                    "Check your internet connection or try again later.\n ${snapshot.error.toString()}",
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then(
+    (value) => runApp(
+      MaterialApp(
+        theme: ThemeData(
+          backgroundColor: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: FutureBuilder(
+          future: getServerLink(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Scaffold(
+                body: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Text(
+                      "Check your internet connection or try again later.\n ${snapshot.error.toString()}",
+                    ),
                   ),
                 ),
+              );
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              return MultiBlocProvider(
+                providers: [
+                  BlocProvider<AuthBloc>(
+                    create: (context) => AuthBloc(
+                      userRepository:
+                          UserRepository(apiUrl: snapshot.data ?? "error"),
+                    ),
+                  ),
+                  BlocProvider<NavBloc>(
+                    create: (context) => NavBloc(),
+                  ),
+                ],
+                child: const HomePage(),
+              );
+            }
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
               ),
             );
-          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            return MultiBlocProvider(
-              providers: [
-                BlocProvider<AuthBloc>(
-                  create: (context) => AuthBloc(
-                    userRepository:
-                        UserRepository(apiUrl: snapshot.data ?? "error"),
-                  ),
-                ),
-                BlocProvider<NavBloc>(
-                  create: (context) => NavBloc(),
-                ),
-              ],
-              child: const HomePage(),
-            );
-          }
-          return const Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        },
+          },
+        ),
       ),
     ),
   );
