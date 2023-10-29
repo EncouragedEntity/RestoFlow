c4c7d442a778c0aa4eeb00cb553146ae5fbf2e6a
EncouragedEntity <arturnitsevich938@gmail.com>
2023-10-29 20:05:33 +0200

    User update

--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -3,7 +3,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:logger/logger.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:resto_flow/data/constants.dart';
 import 'package:resto_flow/models/auth/auth_response_dto.dart';
 import 'package:resto_flow/models/auth/user.dart';
@@ -22,10 +21,6 @@ class UserRepository {
 
   /// Logs in a user using [email] and [password]
   Future<User> logIn(String email, String password) async {
-    if (!await Permission.storage.isGranted) {
-      await Permission.manageExternalStorage.request();
-    }
-
     final response = await http.post(
       Uri.parse(hostname! + authLoginUrl),
       body: jsonEncode({
@@ -46,17 +41,16 @@ class UserRepository {
         value: responseDto.authTokenDTO.accessToken,
       );
 
-      Logger().i(
-        await storage.read(key: "accessToken"),
-      );
-
       return currentUser!;
     } else {
+      Logger().e('Failed to sign in: ${response.reasonPhrase}');
+      Logger().e(response.body);
+
       throw Exception('Failed to sign in: ${response.reasonPhrase}');
     }
   }
 
-  /// Automatic login by saved "accessToken"
+  /// Automatic login by saved "accessToken" from secure storage
   Future<User?> automaticLogin() async {
     const storage = FlutterSecureStorage();
 
@@ -80,10 +74,28 @@ class UserRepository {
     }
   }
 
+  /// Gets userDto from db by it's [id]
   Future<User?> getUserById(int id) async {
     final response = await http.get(
-      Uri.parse("${hostname!}$authUserId/$id"),
+      Uri.parse("${hostname!}$authUserAccount/$id"),
       headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(utf8.decode(response.bodyBytes));
+      return User.fromJson(json);
+    }
+    return null;
+  }
+
+  /// Gets userDto from db by it's [email]
+  Future<User?> getUserByEmail(String email) async {
+    final response = await http.get(
+      Uri.parse("${hostname!}$authUserAccount/email"),
+      headers: {
+        'Content-Type': 'application/json',
+        'email': email,
+      },
     );
 
     if (response.statusCode == 200) {
@@ -104,7 +116,7 @@ class UserRepository {
       password: password,
     );
 
-    final userJson = user.toJson();
+    final userJson = user.toJsonString();
 
     final response = await http.post(
       Uri.parse(hostname! + authRegisterUrl),
@@ -119,6 +131,32 @@ class UserRepository {
     } else {
       throw Exception('Failed to sign up: ${response.reasonPhrase}');
     }
+  }
+
+  Future<User> update(String email, User newUserData) async {
+    final existingUser = await getUserByEmail(email);
+    if (existingUser != null) {
+      final updatedUser = existingUser.copyWith(
+        username: newUserData.username,
+        email: newUserData.email,
+        firstName: newUserData.firstName,
+        lastName: newUserData.lastName,
+        password: newUserData.password,
+      );
+      final response = await http.put(
+        Uri.parse("${hostname!}$authUserAccount/$email"),
+        body: updatedUser.toJsonString(),
+        headers: {'Content-Type': 'application/json'},
+      );
+      if (response.statusCode == 200) {
+        Logger().i("Updated succesfully!");
+        Logger().i(updatedUser.toJsonString());
+        UserRepository.currentUser = updatedUser;
+
+        return updatedUser;
+      }
+    }
+    throw Exception("User with email $email not found.");
   }
 
   /// Logs user out of the system
