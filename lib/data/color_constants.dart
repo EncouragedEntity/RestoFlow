11279a8123e5f6c3fe1b89229c0c302d219824a4
EncouragedEntity <arturnitsevich938@gmail.com>
2023-11-05 00:37:28 +0200

    Divided constants

---
+++ lib/data/color_constants.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+// Dark
+// --text: #fef1f1;
+// --background: #1e0000;
+// --primary: #8e0101;
+// --secondary: #520019;
+// --accent: #ca9000;
+//
+
+const darkPrimaryColor = Color(0xFF8E0101);
+const darkAccentColor = Color(0xFFCA9000);
+
+ThemeData appTheme = ThemeData.dark().copyWith(
+  scaffoldBackgroundColor: const Color.fromARGB(255, 64, 0, 0),
+  primaryColor: darkPrimaryColor,
+  primaryColorLight: darkPrimaryColor,
+  primaryColorDark: darkPrimaryColor,
+  highlightColor: darkAccentColor,
+  appBarTheme: const AppBarTheme(
+    backgroundColor: darkPrimaryColor,
+    foregroundColor: Colors.white,
+  ),
+  textTheme: const TextTheme(
+    bodyLarge: TextStyle(
+      color: Color(0xFFFEF1F1),
+    ),
+    bodyMedium: TextStyle(
+      color: Color(0xFFFEF1F1),
+    ),
+  ),
+);
