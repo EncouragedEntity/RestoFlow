1a8e5d1915e6eee5366d98bd9843c3d595a4f630
EncouragedEntity <arturnitsevich938@gmail.com>
2023-11-15 20:01:00 +0200

    Table reset (App restart)

--- lib/pages/qr_scanner_page.dart
+++ lib/pages/qr_scanner_page.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:logger/logger.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:restart_app/restart_app.dart';
 import 'package:resto_flow/blocs/events/order_event.dart';
 import 'package:resto_flow/blocs/nav_bloc.dart';
 import 'package:resto_flow/blocs/order_bloc.dart';
 import 'package:resto_flow/blocs/states/order_state.dart';
 import 'package:resto_flow/data/string_constants.dart';
+import 'package:resto_flow/widgets/my_themed_button.dart';
 
 import '../blocs/events/nav_event.dart';
+import '../generated/l10n.dart';
+import '../repositories/order_repository.dart';
 
 class ScannerPage extends StatefulWidget {
   const ScannerPage({super.key});
@@ -21,7 +25,6 @@ class _ScannerPageState extends State<ScannerPage> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   String scanResult = "";
   QRViewController? controller;
-  bool didScanCode = false;
 
   void _onQRViewCreated(QRViewController controller) {
     this.controller = controller;
@@ -31,25 +34,27 @@ class _ScannerPageState extends State<ScannerPage> {
         return;
       }
 
-      if (didScanCode) {
-        return;
-      }
-
-      didScanCode = true;
-
       scanResult = scanData.code ?? "";
       if (!scanResult.startsWith(qrPrefix)) {
-        didScanCode = false;
         Logger().e("$scanResult does not start with $qrPrefix");
-        if (!ScaffoldMessenger.of(context).mounted) {
+        if (ScaffoldMessenger.of(context).mounted) {
           ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text("Could't read qr code"), //TODO change to locale
+            SnackBar(
+              backgroundColor:
+                  Theme.of(context).scaffoldBackgroundColor.withGreen(100),
+              content: Text(
+                S.current.qr_scan_error,
+                style: TextStyle(
+                  color: Theme.of(context).highlightColor,
+                ),
+              ),
             ),
           );
         }
         return;
       }
+
+      ScaffoldMessenger.of(context).clearSnackBars();
 
       final tableId = scanResult.substring(10);
 
@@ -100,6 +105,23 @@ class _ScannerPageState extends State<ScannerPage> {
             ],
           ),
         ),
+        if (OrderRepository().currentOrder?.tableId.isNotEmpty ?? false)
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 300),
+              child: Container(
+                constraints: const BoxConstraints(
+                  maxWidth: 150,
+                ),
+                child: MyThemedButton(
+                  onPressed: () {
+                    Restart.restartApp();
+                  },
+                  child: Text(S.current.reset_table),
+                ),
+              ),
+            ),
+          ),
       ],
     );
   }
